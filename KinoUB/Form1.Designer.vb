35ca95dbf513803d32a4bc0aab95c0bc51b1a398
j2ghz <j2.00ghz@gmail.com>
2014-06-03 20:12:42 +0200

    Movie detail download

---
+++ KinoUB/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.LoadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MainDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MainDataSet = New KinoUB.mainDataSet()
+        Me.TableBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TableTableAdapter = New KinoUB.mainDataSetTableAdapters.TableTableAdapter()
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NazevDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PopisDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.MainDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MainDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NazevDataGridViewTextBoxColumn, Me.PopisDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.TableBindingSource
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 24)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(667, 431)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(667, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'LoadToolStripMenuItem
+        '
+        Me.LoadToolStripMenuItem.Name = "LoadToolStripMenuItem"
+        Me.LoadToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+        Me.LoadToolStripMenuItem.Text = "Load"
+        '
+        'MainDataSetBindingSource
+        '
+        Me.MainDataSetBindingSource.DataSource = Me.MainDataSet
+        Me.MainDataSetBindingSource.Position = 0
+        '
+        'MainDataSet
+        '
+        Me.MainDataSet.DataSetName = "mainDataSet"
+        Me.MainDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TableBindingSource
+        '
+        Me.TableBindingSource.DataMember = "Table"
+        Me.TableBindingSource.DataSource = Me.MainDataSetBindingSource
+        '
+        'TableTableAdapter
+        '
+        Me.TableTableAdapter.ClearBeforeFill = True
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "Id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "Id"
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        '
+        'NazevDataGridViewTextBoxColumn
+        '
+        Me.NazevDataGridViewTextBoxColumn.DataPropertyName = "nazev"
+        Me.NazevDataGridViewTextBoxColumn.HeaderText = "nazev"
+        Me.NazevDataGridViewTextBoxColumn.Name = "NazevDataGridViewTextBoxColumn"
+        '
+        'PopisDataGridViewTextBoxColumn
+        '
+        Me.PopisDataGridViewTextBoxColumn.DataPropertyName = "popis"
+        Me.PopisDataGridViewTextBoxColumn.HeaderText = "popis"
+        Me.PopisDataGridViewTextBoxColumn.Name = "PopisDataGridViewTextBoxColumn"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(667, 455)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.MainDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MainDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents LoadToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MainDataSetBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents MainDataSet As KinoUB.mainDataSet
+    Friend WithEvents TableBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents TableTableAdapter As KinoUB.mainDataSetTableAdapters.TableTableAdapter
+    Friend WithEvents IdDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents NazevDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents PopisDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+
+End Class
---
+++ KinoUB/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'MainDataSet.Table' table. You can move, or remove it, as needed.
+        Me.TableTableAdapter.Fill(Me.MainDataSet.Table)
+
+    End Sub
+
+    Private Sub LoadToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadToolStripMenuItem.Click
+        'For i = 2000 To 4000
+        MsgBox(stahni(3056)) 'i)
+        'Next
+    End Sub
+
+    Private Function stahni(i As Integer) As String
+        Dim sr As New StreamReader(Download("http://kino.ub.cz/detailfilmu.php?id=" & i), System.Text.Encoding.GetEncoding(1250))
+        While Not sr.ReadLine = "<td VALIGN=TOP WIDTH=""400"" BGCOLOR=""#000000"" class=btxt>"
+        End While
+
+        Return StripTags(sr.ReadToEnd)
+    End Function
+
+    Private Function Download(p1 As String) As String
+        Dim cesta As String = IO.Path.GetTempFileName()
+        My.Computer.Network.DownloadFile(p1, cesta, "", "", True, 1000, True, FileIO.UICancelOption.ThrowException)
+        Return cesta
+    End Function
+
+    Function StripTags(ByVal html As String) As String
+        ' Remove HTML tags.
+        Return Regex.Replace(html, "<.*?>", "")
+    End Function
+    'remove empty lines
+    'add to database
+
+End Class
