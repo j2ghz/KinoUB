60d4300632e36a8fbafae1788cbf45ea50877740
j2ghz <j2.00ghz@gmail.com>
2014-06-03 20:28:11 +0200

    removed empty lines in debug mgbox

--- KinoUB/Form1.vb
+++ KinoUB/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO
 Imports System.Net
 Imports System.Text.RegularExpressions
+Imports System.Text
 
 Public Class Form1
 
@@ -13,19 +14,19 @@ Public Class Form1
 
     Private Sub LoadToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadToolStripMenuItem.Click
         'For i = 2000 To 4000
-        MsgBox(stahni(3056)) 'i)
+        MsgBox(remove(downloadmovie(3056))) 'i)
         'Next
     End Sub
 
-    Private Function stahni(i As Integer) As String
-        Dim sr As New StreamReader(Download("http://kino.ub.cz/detailfilmu.php?id=" & i), System.Text.Encoding.GetEncoding(1250))
+    Private Function downloadmovie(i As Integer) As String
+        Dim sr As New StreamReader(Downloadfile("http://kino.ub.cz/detailfilmu.php?id=" & i), System.Text.Encoding.GetEncoding(1250))
         While Not sr.ReadLine = "<td VALIGN=TOP WIDTH=""400"" BGCOLOR=""#000000"" class=btxt>"
         End While
 
         Return StripTags(sr.ReadToEnd)
     End Function
 
-    Private Function Download(p1 As String) As String
+    Private Function Downloadfile(p1 As String) As String
         Dim cesta As String = IO.Path.GetTempFileName()
         My.Computer.Network.DownloadFile(p1, cesta, "", "", True, 1000, True, FileIO.UICancelOption.ThrowException)
         Return cesta
@@ -37,4 +38,17 @@ Public Class Form1
     End Function
     'remove empty lines
     'add to database
+
+    Private Function remove(p1 As String) As Object
+        Dim sb As New StringBuilder
+        For Each s As String In p1.Split(vbCr, vbCrLf, vbLf, Environment.NewLine)
+            If Not s = "" Then
+                sb.AppendLine(s)
+            End If
+        Next
+        Return sb.ToString
+    End Function
+
+
+
 End Class
