0dad3b5b4faabade9435f2089a7ad96e000ca26e
j2ghz <j2.00ghz@gmail.com>
2014-06-04 07:55:56 +0200

    testing jenkins

--- KinoUB/Form1.vb
+++ KinoUB/Form1.vb
@@ -7,7 +7,6 @@ Public Class Form1
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'MainDataSet.Table' table. You can move, or remove it, as needed.
         Me.TableTableAdapter.Fill(Me.MainDataSet.Movies)
 
     End Sub
